0ff0ddc02e012113f45f1c5eda4879828fedd138
Andrew J. Pounds <pounds_aj@mercer.edu>
2016-10-25 08:29:01 -0400

    added prototypes

---
+++ prototypes.h
@@ -0,0 +1,16 @@
+
+
+float dot( float, float, float, float, float, float );
+float vlength( float, float, float, float, float, float );
+
+void buildTranslate( float, float, float, float*);
+void buildIdentity(float, float* );
+void buildRotateX(float, float* );
+void buildRotateY(float, float* );
+void buildRotateZ(float, float* );
+
+void vmatm( int, float*, float* );
+void mmatm( int, float*, float* );
+void transpose ( int, float* );
+
+
